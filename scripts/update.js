aac538967b34cf450deb1b654124e020b3cdf9ad
Liang Xingchen <liang@maichong.it>
2017-04-13 14:29:14 +0800

    k8s-dns-kube-dns-amd64

--- scripts/update.js
+++ scripts/update.js
@@ -28,6 +28,7 @@ const otherImages = {
     'kubernetes-dashboard-amd64': ['v1.5.1'],
     'nginx-ingress-controller': ['0.9.0-beta.3'],
     'defaultbackend-amd64': ['1.3'],
+    'k8s-dns-kube-dns-amd64': ['1.14.1'],
 };
 
 function update(image, tag) {
