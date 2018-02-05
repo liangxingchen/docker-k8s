d9bf37b3f25843a26f225293b1450cbe420842f7
ZhaoLei <zhao@maichong.it>
2018-02-05 16:56:17 +0800

    1.9.2

--- scripts/update.js
+++ scripts/update.js
@@ -17,26 +17,26 @@ const kubeImages = [
     'kube-scheduler-amd64'
 ];
 
-const kubeTags = ['v1.5.3', 'v1.6.1', 'v1.6.3', 'v1.6.4', 'v1.8.1'];
+const kubeTags = ['v1.5.3', 'v1.6.1', 'v1.6.3', 'v1.6.4', 'v1.8.1', 'v1.9.2'];
 
 const otherImages = {
-    'etcd-amd64': ['3.0.17', '3.1.10'],
+    'etcd-amd64': ['3.0.17', '3.1.10', '3.2.14'],
     'kube-discovery-amd64': ['1.0'],
     'kube-dnsmasq-amd64': ['1.4', '1.4.1'],
     'kubectl': ['v1.0.7'],
     'kubedns-amd64': ['1.9'],
-    'pause-amd64': ['3.0'],
-    'kubernetes-dashboard-amd64': ['v1.5.1', 'v1.6.0', 'v1.7.1'],
+    'pause-amd64': ['3.0', '3.1'],
+    'kubernetes-dashboard-amd64': ['v1.5.1', 'v1.6.0', 'v1.7.1', 'v1.8.2'],
     'nginx-ingress-controller': ['0.9.0-beta.3', '0.9.0-beta.5'],
     'nginx-ingress-controller-amd64': ['0.9.0-beta.15'],
-    'defaultbackend-amd64': ['1.3'],
-    'k8s-dns-kube-dns-amd64': ['1.14.1', '1.14.2', '1.14.6'],
-    'k8s-dns-dnsmasq-amd64': ['1.14.1', '1.14.2', '1.14.6'],
-    'k8s-dns-dnsmasq-nanny-amd64': ['1.14.1', '1.14.2', '1.14.6'],
-    'k8s-dns-sidecar-amd64': ['1.14.1', '1.14.2', '1.14.6'],
+    'defaultbackend-amd64': ['1.3', '1.4'],
+    'k8s-dns-kube-dns-amd64': ['1.14.1', '1.14.2', '1.14.6', '1.14.8'],
+    'k8s-dns-dnsmasq-amd64': ['1.14.1', '1.14.2', '1.14.6', '1.14.8'],
+    'k8s-dns-dnsmasq-nanny-amd64': ['1.14.1', '1.14.2', '1.14.6', '1.14.8'],
+    'k8s-dns-sidecar-amd64': ['1.14.1', '1.14.2', '1.14.6', '1.14.8'],
     'exechealthz-amd64': ['v1.2.0'],
-    'fluentd-elasticsearch': ['1.22', 'v2.0.1'],
-    'elasticsearch': ['v2.4.1-2', 'v5.6.2'],
+    'fluentd-elasticsearch': ['1.22', 'v2.0.1', 'v2.0.3'],
+    'elasticsearch': ['v2.4.1-2', 'v5.6.2', 'v5.6.4'],
     'kibana': ['v4.6.1-1', 'v5.4.0'],
 };
 
